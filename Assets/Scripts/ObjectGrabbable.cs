5550ff92e8992172b12a13bddaee1b6fc31f7076
Hashir <50779880+Hashir103@users.noreply.github.com>
2025-12-04 15:27:33 -0500

    fixed pick up cooked food

--- Assets/Scripts/ObjectGrabbable.cs
+++ Assets/Scripts/ObjectGrabbable.cs
@@ -12,24 +12,36 @@ public class ObjectGrabbable : NetworkBehaviour
     }
     public void TryGrab(Transform grabPoint)
     {
+        Debug.Log("[ObjectGrabbable] TryGrab requested by client for " + name);
         // Request the server to assign ownership
         RequestGrabServerRpc(NetworkManager.Singleton.LocalClientId);
 
         // Store who wants to grab it
         objectGrabPointTransform = grabPoint;
         objectRigidbody.isKinematic = true;
+        if (objectRigidbody != null)
+        {
+            objectRigidbody.useGravity = false;
+        }
     }
 
     public void TryDrop()
     {
+        Debug.Log("[ObjectGrabbable] TryDrop requested by client for " + name);
         RequestDropServerRpc();
         objectGrabPointTransform = null;
         objectRigidbody.isKinematic = false;
+        if (objectRigidbody != null)
+        {
+            objectRigidbody.useGravity = true;
+        }
     }
 
+    #pragma warning disable 0618
     [ServerRpc(RequireOwnership = false)]
     private void RequestGrabServerRpc(ulong clientId)
     {
+        Debug.Log("[ObjectGrabbable][ServerRpc] ChangeOwnership to client=" + clientId);
         // Server gives the client ownership of the object
         GetComponent<NetworkObject>().ChangeOwnership(clientId);
     }
@@ -41,6 +53,7 @@ public class ObjectGrabbable : NetworkBehaviour
         if (netObj == null || !netObj.IsSpawned) 
             return; 
 
+        Debug.Log("[ObjectGrabbable][ServerRpc] RemoveOwnership for " + name);
         netObj.RemoveOwnership();
     }
 
@@ -50,9 +63,37 @@ public class ObjectGrabbable : NetworkBehaviour
     {
         objectGrabPointTransform = null;
         if (objectRigidbody != null)
+        {
             objectRigidbody.isKinematic = false;
-        transform.parent = null;
+            objectRigidbody.useGravity = true;
+        }
+
+        NetworkObject netObj = GetComponent<NetworkObject>();
+        if (netObj == null) return;
+
+        if (IsServer)
+        {
+            Debug.Log("[ObjectGrabbable] Server force drop, clearing parent.");
+            netObj.TrySetParent((Transform)null, false);
+        }
+        else
+        {
+            Debug.Log("[ObjectGrabbable] Client requests server force drop.");
+            ForceDropServerRpc();
+        }
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ForceDropServerRpc()
+    {
+        NetworkObject netObj = GetComponent<NetworkObject>();
+        if (netObj != null)
+        {
+            Debug.Log("[ObjectGrabbable][ServerRpc] Clearing parent on server.");
+            netObj.TrySetParent((Transform)null, false);
+        }
+    }
+    #pragma warning restore 0618
 
     private void FixedUpdate()
     {
