64358b4c83e765ac1c28a303ad87171a767077d6
Jack-Caldarone <jackcaldarone@gmail.com>
2025-12-04 15:08:13 -0500

    what i have

--- Assets/Scripts/TagAssignment.cs
+++ Assets/Scripts/TagAssignment.cs
@@ -1,10 +1,14 @@
 using Unity.Netcode;
 using UnityEngine;
+using Unity.Behavior;
 
 public class TagAssignment : NetworkBehaviour
 {
     public static GameObject Player1;
     public static GameObject Player2;
+    // [SerializeField] private BehaviorGraphAgent agent;
+    // private string p1 = "Player1";
+    // private string p2 = "Player2";
 
     public override void OnNetworkSpawn()
     {
@@ -27,8 +31,14 @@ public class TagAssignment : NetworkBehaviour
         gameObject.tag = tag;
 
         if (tag == "P1")
+        {
             Player1 = gameObject;
+            // agent.SetVariableValue<GameObject>(p1, gameObject);   
+        }
         else if (tag == "P2")
+        {
             Player2 = gameObject;
+            // agent.SetVariableValue<GameObject>(p2, gameObject);
+        }
     }
 }
